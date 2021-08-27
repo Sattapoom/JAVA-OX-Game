7e40c3ab9844f3dc0bf844aaeb6c316507176f9a
SattapoomTulyasuk <s6301012630192@kmutnb.ac.th>
2021-08-27 18:33:08 +0700

    Draw munu,playing screen,win screen

--- OX.java
+++ OX.java
@@ -1,38 +1,29 @@
-import java.util.Scanner;
 import java.util.Arrays;
+import java.awt.event.*;
+import java.awt.*;
+import javax.swing.*;
 
-public class OX{
+public class OX extends JFrame implements MouseListener{
 
     String[][] board_array;
     String player = "X";
     int turn_count = 0;
-    int size;
+    int board_size = 5;
 
 //=========================================================================
-    void setSize(int s){
-        size = s;
+    void setBoardboard_size(int s){
+        board_size = s;
         board_array = new String[s][s];
-        for(int i=0;i<size;i++){
-            for(int j=0;j<size;j++){
+        for(int i=0;i<board_size;i++){
+            for(int j=0;j<board_size;j++){
             board_array[i][j] = " ";
             }
         }
     }
-    void display_board() {
-        for (int i=0;i<size;++i){
-            for (int j=0;j<size;++j){
-                System.out.print("("+board_array[i][j]+")");
-            }
-            System.out.print("\n");
-        }
-    }
 
-    Boolean add_position(Integer pos) {
+    Boolean add_position(Integer row,Integer col) {
 
-        int row = (pos-1)/size;
-        int col = (pos-1)%size;
         if(board_array[row][col] == "X" || board_array[row][col] == "O"){
-            System.out.print("Fail, This position has been chosen. plese try again : ");
             return false;
         } else {
             board_array[row][col] = this.player;
@@ -41,25 +32,25 @@ public class OX{
     }
 
     Boolean check_winner(){
-        String[] checker = new String[size];
-        for (int i=0;i<size;i++){
+        String[] checker = new String[board_size];
+        for (int i=0;i<board_size;i++){
             checker[i] = this.player;
         }
-        String[] temp_hor = new String[size];
-        String[] temp_ver = new String[size];
-        for (int i=0;i<size;i++){
+        String[] temp_hor = new String[board_size];
+        String[] temp_ver = new String[board_size];
+        for (int i=0;i<board_size;i++){
             if(Arrays.deepEquals(checker, board_array[i])){
                 return true;
             }
-            String[] temp = new String[size];
-            for (int j=0;j<size;j++){
+            String[] temp = new String[board_size];
+            for (int j=0;j<board_size;j++){
                 temp[j] = board_array[j][i];
             }
             if(Arrays.deepEquals(checker, temp)){
                 return true;
             }
             temp_hor[i] = board_array[i][i];
-            temp_ver[i] = board_array[i][(size-1)-i];
+            temp_ver[i] = board_array[i][(board_size-1)-i];
         }
         if(Arrays.deepEquals(checker, temp_hor) || Arrays.deepEquals(checker, temp_ver)){
             return true;
@@ -85,25 +76,132 @@ public class OX{
         return turn_count;
     }
 //=========================================================================
+    void Convert_mousepos(int mouse_x,int mouse_y){
+        int row,col;
+        row = (int)mouse_y/(sc_size/board_size);
+        col = (int)mouse_x/(sc_size/board_size);
+        System.out.println(row +" "+col);
+    }
+    //-----------UESLESS--################
+    @Override
+    public void mouseClicked(MouseEvent e) {}
+    @Override
+    public void mousePressed(MouseEvent e) {
+        if (game_mode.equals("Menu") && e.getX() > 250 && e.getX() < 550 && e.getY() >450 && e.getY()<550){
+            Graphics2D g2d = (Graphics2D) screen.getGraphics();
+            g2d.setColor(Color.decode("#33D81A"));
+            g2d.fillRect(250,450,300,100);
+            g2d.setColor(Color.black);
+            g2d.drawRect(250,450,300,100);
+            g2d.setFont(new Font("Calibri", Font.PLAIN, 50));
+            g2d.drawString("Start",340,515);
+            g2d.setFont(new Font("Calibri", Font.PLAIN, 20));
+            g2d.drawString("Insert board size.",320,290);
 
-//=========================================================================
-    public static void main(String[] args) {
-        OX game = new OX();
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Size : ");
-        int s = sc.nextInt();
-        game.setSize(s);
-        game.display_board();
-        while(game.getTurncount() < s*s ){
-            System.out.print("Pos "+game.getPlayingmark()+": ");
-            while(!game.add_position(sc.nextInt()));
-            game.display_board();
-            if(game.check_winner()){
-                System.out.println("Win");
-                break;
+            String str_size = tf1.getText();
+            int int_size = Integer.parseInt(str_size);
+            if (int_size>1){
+                board_size = int_size;
             }
-            game.change_player();
         }
-        sc.close();
+        else if(game_mode.equals("Play")){
+            Convert_mousepos(e.getX(),e.getY());
         }
     }
+    @Override
+    public void mouseEntered(MouseEvent e) {}
+    @Override
+    public void mouseExited(MouseEvent e) {}
+    //------MOUSE------MOUSE------MOUSE------MOUSE------MOUSE------MOUSE------
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        if (game_mode.equals("Menu") && e.getX() > 250 && e.getX() < 550 && e.getY() >450 && e.getY()<550){
+            repaint();
+            System.out.println(board_size);
+        }
+    }
+    //=========================================================================
+    JPanel screen = new JPanel();
+    String game_mode = "Play";
+    final int sc_size = 800;
+
+    void Draw_game(){
+        int table_size = sc_size/board_size;
+        screen.removeAll();
+        Graphics2D g2d = (Graphics2D) screen.getGraphics();
+        g2d.setStroke(new BasicStroke(2));
+        for (int i=0;i<board_size;i++){
+            g2d.setColor(Color.black);
+            g2d.drawLine(0, i*table_size, sc_size, i*table_size);
+            for (int j=0;j<board_size;j++){
+                g2d.setColor(Color.black);
+                g2d.drawLine(j*table_size, 0, j*table_size, sc_size);
+                g2d.setColor(Color.blue);
+                g2d.setFont(new Font("Calibri", Font.PLAIN, 150));
+            }
+        }
+    }
+    JTextField tf1;
+    void Draw_menu(){
+        
+        Graphics2D g2d = (Graphics2D) screen.getGraphics();
+        g2d.setStroke(new BasicStroke(3));
+
+        g2d.setColor(Color.decode("#2AEA0E"));
+        g2d.fillRect(250,450,300,100);
+        g2d.setColor(Color.black);
+        g2d.drawRect(250,450,300,100);
+        g2d.setFont(new Font("Calibri", Font.PLAIN, 50));
+        g2d.drawString("Start",340,515);
+        g2d.setFont(new Font("Calibri", Font.PLAIN, 20));
+        g2d.drawString("Insert board size.",320,290);
+    }
+    void Draw_win(){
+        Graphics2D g2d = (Graphics2D) screen.getGraphics();
+        g2d.setColor(Color.ORANGE);
+        g2d.setFont(new Font("Calibri", Font.PLAIN, 150));
+        g2d.drawString("You Win",150,325);
+        g2d.setColor(Color.gray);
+        g2d.setFont(new Font("Calibri", Font.PLAIN, 20));
+        g2d.drawString("Click for back to main menu.",300,500);
+    }
+    //------------------################
+    public void paint(Graphics g){
+        switch (game_mode) {
+            // super.paint(g);
+            case "Menu":
+                Draw_menu();
+                break;
+            case "Play":
+                Draw_game();
+                break;
+            case "Win":  
+                Draw_win();
+                break;
+            default:
+                break;
+        }
+    }
+
+//------------------################
+    OX(){
+        this.setTitle("Sorting Game");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
+        this.setResizable(false);
+
+        screen.setPreferredSize(new Dimension(sc_size, sc_size));
+        screen.addMouseListener(this);
+        this.add(screen);
+        this.pack();
+        this.setVisible(true);
+
+        tf1 = new JTextField();
+        tf1.setBounds(250,300,300,100);
+        tf1.setFont(new Font("Calibri", Font.PLAIN, 50));
+        screen.add(tf1);
+    }
+//=========================================================================
+    public static void main(String[] args) {
+        new OX();
+    }
+}
