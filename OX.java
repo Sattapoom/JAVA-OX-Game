5e611421595f4de8987718b1f700f45de656f769
SattapoomTulyasuk <s6301012630192@kmutnb.ac.th>
2021-08-27 21:10:07 +0700

    Complete version.

--- OX.java
+++ OX.java
@@ -8,7 +8,7 @@ public class OX extends JFrame implements MouseListener{
     String[][] board_array;
     String player = "X";
     int turn_count = 0;
-    int board_size = 5;
+    int board_size;
 
 //=========================================================================
     void setBoardboard_size(int s){
@@ -21,20 +21,25 @@ public class OX extends JFrame implements MouseListener{
         }
     }
 
-    Boolean add_position(Integer row,Integer col) {
+    void add_position(Integer row,Integer col) {
 
         if(board_array[row][col] == "X" || board_array[row][col] == "O"){
-            return false;
+            System.out.println("NOPE");
         } else {
             board_array[row][col] = this.player;
-            return true;
+            change_player();
         }
     }
 
     Boolean check_winner(){
         String[] checker = new String[board_size];
         for (int i=0;i<board_size;i++){
-            checker[i] = this.player;
+            if(this.player.equals("X")){
+                checker[i] = "O";
+            }
+            else{
+                checker[i] = "X";
+            }
         }
         String[] temp_hor = new String[board_size];
         String[] temp_ver = new String[board_size];
@@ -69,18 +74,24 @@ public class OX extends JFrame implements MouseListener{
     }
 
     String getPlayingmark(){
-        return player;
+        if (player.equals("X")){
+            return "O";
+        }
+        else{
+            return "X";
+        }
     }
 
     Integer getTurncount(){
         return turn_count;
     }
 //=========================================================================
-    void Convert_mousepos(int mouse_x,int mouse_y){
-        int row,col;
-        row = (int)mouse_y/(sc_size/board_size);
-        col = (int)mouse_x/(sc_size/board_size);
+    int[] Convert_mousepos(int mouse_x,int mouse_y){
+        int[] row_col = {(int)mouse_y/(sc_size/board_size),(int)mouse_x/(sc_size/board_size)};
+        int row = (int)mouse_y/(sc_size/board_size);
+        int col = (int)mouse_x/(sc_size/board_size);
         System.out.println(row +" "+col);
+        return row_col;
     }
     //-----------UESLESS--################
     @Override
@@ -102,10 +113,11 @@ public class OX extends JFrame implements MouseListener{
             int int_size = Integer.parseInt(str_size);
             if (int_size>1){
                 board_size = int_size;
+                setBoardboard_size(board_size);
             }
-        }
-        else if(game_mode.equals("Play")){
-            Convert_mousepos(e.getX(),e.getY());
+            else{
+                tf1.setText("Can not");
+            }
         }
     }
     @Override
@@ -117,12 +129,37 @@ public class OX extends JFrame implements MouseListener{
     public void mouseReleased(MouseEvent e) {
         if (game_mode.equals("Menu") && e.getX() > 250 && e.getX() < 550 && e.getY() >450 && e.getY()<550){
             repaint();
-            System.out.println(board_size);
+            if (board_size>1){
+                game_mode = "Background";
+                repaint();
+                game_mode = "Play";
+                screen.removeAll();
+            }
+        }
+        else if(game_mode.equals("Play")){
+            int[] row_col = Convert_mousepos(e.getX(),e.getY());
+            add_position(row_col[0],row_col[1]);
+            if(check_winner()){
+                game_mode = "Win";
+            }
+            repaint();
+        }
+        else if(game_mode.equals("Win")){
+            
+            game_mode = "Play";
+            turn_count = 0;
+            setBoardboard_size(board_size);
+            screen.removeAll();
+            repaint();
+            tf1 = new JTextField();
+            tf1.setBounds(250,300,300,100);
+            tf1.setFont(new Font("Calibri", Font.PLAIN, 50));
+            repaint();
         }
     }
     //=========================================================================
     JPanel screen = new JPanel();
-    String game_mode = "Play";
+    String game_mode = "Menu";
     final int sc_size = 800;
 
     void Draw_game(){
@@ -137,7 +174,8 @@ public class OX extends JFrame implements MouseListener{
                 g2d.setColor(Color.black);
                 g2d.drawLine(j*table_size, 0, j*table_size, sc_size);
                 g2d.setColor(Color.blue);
-                g2d.setFont(new Font("Calibri", Font.PLAIN, 150));
+                g2d.setFont(new Font("Calibri", Font.PLAIN, table_size));
+                g2d.drawString(board_array[i][j],(table_size*j)+20,(table_size*(i+1))-20);
             }
         }
     }
@@ -157,13 +195,14 @@ public class OX extends JFrame implements MouseListener{
         g2d.drawString("Insert board size.",320,290);
     }
     void Draw_win(){
+        screen.removeAll();
         Graphics2D g2d = (Graphics2D) screen.getGraphics();
         g2d.setColor(Color.ORANGE);
         g2d.setFont(new Font("Calibri", Font.PLAIN, 150));
-        g2d.drawString("You Win",150,325);
+        g2d.drawString(getPlayingmark()+" Win",150,325);
         g2d.setColor(Color.gray);
         g2d.setFont(new Font("Calibri", Font.PLAIN, 20));
-        g2d.drawString("Click for back to main menu.",300,500);
+        g2d.drawString("Click for play again.",300,500);
     }
     //------------------################
     public void paint(Graphics g){
@@ -173,10 +212,15 @@ public class OX extends JFrame implements MouseListener{
                 Draw_menu();
                 break;
             case "Play":
+                super.paint(g);
                 Draw_game();
                 break;
-            case "Win":  
+            case "Win":
+                super.paint(g);
                 Draw_win();
+                break;
+            case "Background":
+                super.paint(g);
                 break;
             default:
                 break;
@@ -185,6 +229,7 @@ public class OX extends JFrame implements MouseListener{
 
 //------------------################
     OX(){
+
         this.setTitle("Sorting Game");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
         this.setResizable(false);
